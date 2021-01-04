ab94dfb51e9a7cf62fef9f88c5064e509c2fdf53
Savepong <savepong.com@gmail.com>
2021-01-04 19:09:49 +0700

    learn about template

--- my-element.js
+++ my-element.js
@@ -12,17 +12,53 @@ class MyElement extends LitElement {
         this.myString = 'Hello World';
         this.myArray = ['an', 'array', 'of', 'test', 'data'];
         this.myBool = true;
+        this.prop1 = 'mydiv';
+        this.prop2 = true;
+        this.prop3 = 'The Cat';
     }
     render() {
         return html `
+      <!-- text binding -->
       <p>${this.myString}</p>
+
+      <!-- attribute binding -->
+      <div id="${this.prop1}">Attribute binding</div>
+
+      <!-- boolean attribute binding -->
+      <div>
+        boolean attribute binding
+        <input type="text" ?disabled="${this.prop2}"/>
+      </div>
+
+      <!-- property binding -->
+      <div>
+        property binding
+        <input type="text" .value="${this.prop3}"/>
+      </div>
+
+      <!-- event handler binding -->
+      <div>
+        event handler binding
+        <button @click="${this.clickHandler}">Click</button>
+      </div>
+
+      <!-- loops -->
       <ul>
         ${this.myArray.map(i => html `<li>${i}</li>`)}
       </ul>
+
+      <!-- conditionals -->
       ${this.myBool
             ? html `<p>Render some HTML if myBool is true</p>`
             : html `<p>Render some other HTML if myBool is false</p>`}
+
+      <!-- slot -->
+      <slot></slot>
+      <slot name="other"></slot>
     `;
+    }
+    clickHandler(e) {
+        alert(e.type);
     }
 }
 customElements.define('my-element', MyElement);
